08d5dddb112069f431d99efface245423a65590d
Share2 MSA <NL12143@hotmail.com>
2026-09-23 14:21:07 +0200

    feat: toevoegen powershell startscripts

---
+++ scripts/NPM=Run Server App.ps1
@@ -0,0 +1,16 @@
+﻿git status 
+
+# 1. Backend packages installeren
+cd "C:\_WOLFFIE\_WOLFFIE-RC\server"
+npm install
+
+cd "C:\_WOLFFIE\_WOLFFIE-RC\server"
+npm run dev
+
+
+# 2. Frontend packages installeren
+cd "C:\_WOLFFIE\_WOLFFIE-RC\my-app"
+npm install
+
+cd "C:\_WOLFFIE\_WOLFFIE-RC\my-app"
+npm run dev
